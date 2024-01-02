2371e629beea6765e0d60fd68e557bd768192612
Mohammed Usman F <83750506+MdUsmanF@users.noreply.github.com>
2024-01-02 14:32:19 +0000

    Old commits

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
 console.log("Hello Usman");
+function sumAll(startIndex, endIndex) {
+    var sum = 0;
+    for (let index = startIndex; index <= endIndex; index++) {
+        sum = sum + index;    
+    }
+    return sum
+}
+
+var sum = sumAll(1,100)
+console.log("The Sum is", sum);
