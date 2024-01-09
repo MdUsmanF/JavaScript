bcaaafb4262ab4d98254696aac551eb820ddf912
Mohammed Usman F <83750506+MdUsmanF@users.noreply.github.com>
2024-01-09 14:34:28 +0000

    Problem 3

---
+++ 01_basics/3.LeftTrainglePattern.js
@@ -0,0 +1,18 @@
+function LeftStarPattern(n,string) {
+    for (let i = 1; i <= n; i++) {
+        // printing spaces
+        for (let j = 0; j > n - i; j++) {
+          string += " ";
+        }
+        // printing star
+        for (let k = 0; k < i; k++) {
+          string += "*";
+        }
+        string += "\n";
+      }
+      return string;
+}
+
+let n = 5;
+let string = "";
+console.log(LeftStarPattern(n,string));
